e57a9346254bd3b6a94156e8b13834d724e36ab3
DuckRainbow <sillestyna@gmail.com>
2025-01-17 18:29:45 +0500

    sample of models

--- retailing/models.py
+++ retailing/models.py
@@ -1,9 +1,67 @@
 from django.db import models
 
 
-class Network(models.Model):
-    pass
+class Supplier(models.Model):
+    title = models.CharField(
+
+    )
+    contact = models.ForeignKey(
+
+    )
+    products = models.ManyToManyField(
+
+    )
+    supplier = models.ForeignKey(
+
+    )
+    debt = models.DecimalField(
+        max_digits=20,
+        decimal_places=2
+    )
+    created_time = models.DateTimeField(
+        auto_created=True
+    )
+    LEVEL_CHOICES = {
+        0: 'завод',
+        1: 'розничная сеть',
+        2: 'индивидуальный предприниматель'
+    }
+    level = models.CharField(
+        choices=LEVEL_CHOICES,
+    )
+
+    class Meta:
+        pass
 
 
-class Link(models.Model):
-    pass
+class Contact(models.Model):
+    email = models.CharField(
+
+    )
+    country = models.CharField(
+
+    )
+    city = models.CharField(
+
+    )
+    street = models.CharField(
+
+    )
+    building = models.CharField(
+
+    )
+
+    class Meta:
+        pass
+
+
+class Product(models.Model):
+    title = models.CharField(
+
+    )
+    type = models.CharField(
+
+    )
+    date_of_release = models.DateTimeField(
+
+    )
