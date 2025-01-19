4268e154a57464e10b946a35714e2f6342ba183f
DuckRainbow <sillestyna@gmail.com>
2025-01-19 16:11:22 +0500

    all in models

--- retailing/models.py
+++ retailing/models.py
@@ -4,31 +4,31 @@ from django.db import models
 class Contact(models.Model):
     email = models.EmailField(
         verbose_name='email',
-        help_text='',
+        help_text='Укажите email контакта',
     )
     country = models.CharField(
         max_length=100,
         verbose_name='страна',
-        help_text='',
+        help_text='Укажите страну контакта',
     )
     city = models.CharField(
         max_length=50,
         verbose_name='город',
-        help_text='',
+        help_text='Укажите город контакта',
     )
     street = models.CharField(
         max_length=50,
         verbose_name='улица',
-        help_text='',
+        help_text='Укажите улицу контакта',
     )
     building = models.CharField(
         max_length=20,
         verbose_name='номер дома',
-        help_text='',
+        help_text='Укажите номер дома/строения контакта',
     )
 
     def __str__(self):
-        return f' '
+        return f'Контакт {self.email}, по адресу {self.country}, г.{self.city} ул.{self.street} д.{self.building}'
 
     class Meta:
         verbose_name = 'Контакт'
@@ -39,21 +39,21 @@ class Product(models.Model):
     title = models.CharField(
         max_length=100,
         verbose_name='название',
-        help_text='',
+        help_text='Укажите название продукта',
     )
     type = models.CharField(
         max_length=25,
         verbose_name='модель',
-        help_text='',
+        help_text='Укажите модель продукта',
     )
     date_of_release = models.DateTimeField(
         auto_now=True,
-        verbose_name='',
-        help_text='дата выхода продукта на рынок',
+        verbose_name='дата выхода продукта на рынок',
+        help_text='Укажите дату выхода продукта на рынок',
     )
 
     def __str__(self):
-        return f' '
+        return f'Продукт {self.title}, модель {self.type}, дата выхода на рынок {self.date_of_release}'
 
     class Meta:
         verbose_name = 'Продукт'
@@ -64,32 +64,32 @@ class Supplier(models.Model):
     title = models.CharField(
         max_length=100,
         verbose_name='Название',
-        help_text='',
+        help_text='Укажите название поставщика',
     )
 
-    contact = models.ManyToManyField(
+    contacts = models.ManyToManyField(
         Contact,
         verbose_name='Контакты',
-        help_text='',
+        help_text='Укажите контакты поставщика',
     )
 
     products = models.ManyToManyField(
         Product,
         verbose_name='Продукты',
-        help_text='',
+        help_text='Укажите продукты поставщика',
     )
 
     supplier = models.ForeignKey(
         'self',
         verbose_name='Поставщик',
-        help_text='',
+        help_text='Укажите поставщика продуктов',
     )
 
     debt = models.DecimalField(
         max_digits=20,
         decimal_places=2,
         verbose_name='Задолженность перед поставщиком',
-        help_text='',
+        help_text='Укажите задолженность перед поставщиком',
     )
 
     created_time = models.DateTimeField(
@@ -108,7 +108,10 @@ class Supplier(models.Model):
     )
 
     def __str__(self):
-        return f' '
+        return (
+            f'Поставщик {self.title}, контакты {self.contacts}, продукты {self.products}, поставщик {self.supplier},'
+            f'задолженность перед поставщиком {self.debt}, время создания {self.created_time}'\
+            )
 
     class Meta:
         verbose_name = 'Поставщик'
