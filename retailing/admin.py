351984174aa41f60b263a44f30d4867d990c6d77
DuckRainbow <sillestyna@gmail.com>
2025-01-19 16:13:05 +0500

    admin for supplier

--- retailing/admin.py
+++ retailing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from retailing.models import Supplier
+
+
 # Register your models here.
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'contacts', 'products', 'supplier', 'debt', 'created_time', 'level')
+    list_filter = ('contact.city',)
