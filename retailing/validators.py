cd0fe5484dcdb27c2d3ea8070cd5174f75fe374d
DuckRainbow <sillestyna@gmail.com>
2025-01-19 16:36:37 +0500

    validator

---
+++ retailing/validators.py
@@ -0,0 +1,16 @@
+from rest_framework.serializers import ValidationError
+
+
+class SupplierValidator:
+    """
+    Данный валидатор проверяет, чтобы при создании поставщика, его уровень был больше(ниже по иерархии),
+    чем уровень его поставщика
+    """
+    def __init__(self, field1, field2):
+        self.level = field1
+        self.supplier = field2
+
+    def __call__(self):
+        if self.level <= self.supplier.level:
+            raise ValidationError('Уровень должен быть больше уровня поставщика.')
+        return None
