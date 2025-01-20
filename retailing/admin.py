fa29dd27639ff7360e6d5cbced912cc0afb934b7
DuckRainbow <sillestyna@gmail.com>
2025-01-20 14:55:31 +0500

    clean debt action for admin

--- retailing/admin.py
+++ retailing/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 from retailing.models import Supplier
 
 
+def clean_debt(pk):
+    supplier = Supplier.objects.filter(id=pk)
+    supplier.debt = 0
+    supplier.save()
+
+
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
     # Представление модели Supplier в admin панели
-    list_display = ('id', 'title', 'contacts', 'products', 'supplier', 'debt', 'created_time', 'level')
+    list_display = ('id', 'title', 'debt', 'created_time', 'level')
+    fields = [('id', 'title', 'level'), 'contacts', 'products', 'supplier', 'debt', 'created_time']
     list_filter = ('contact.city',)
+    actions = [clean_debt, ]
