c69b3eadb94c46f0cf9b7c6f84e73cf90f4cce4a
DuckRainbow <sillestyna@gmail.com>
2025-01-20 13:27:31 +0500

    serializers for users

---
+++ users/serializers.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "phone_num",
+            "email",
+            "password",
+            "image",
+            "token",
+            "uid",
+        )
+
+
+class PasswordResetRequestSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["email"]
+
+    def validate(self, data):
+        email = data["email"]
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({"email": "Указанный email не найден."})
+        return data
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    new_password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+
+    class Meta:
+        model = User
+        fields = ["password"]
