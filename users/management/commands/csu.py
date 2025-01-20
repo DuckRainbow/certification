7183600bacc81f3bb4a2a6ed4cd35ad010cf7d4e
DuckRainbow <sillestyna@gmail.com>
2025-01-20 15:54:45 +0500

    csu

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,7 +5,7 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email='admin@example.com', phone_num='89999999999', first_name='user', last_name='super')
+        user = User.objects.create(email='admin@example.com', first_name='user', last_name='super')
         user.set_password('123qwe')
 
         user.is_active = True
