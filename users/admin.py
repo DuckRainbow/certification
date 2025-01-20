f1ab37efd49b6aed7ac9ce7d78ddccd4b201552a
DuckRainbow <sillestyna@gmail.com>
2025-01-20 13:25:49 +0500

    admin панель для users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class SupplierAdmin(admin.ModelAdmin):
+    # Представление модели Supplier в admin панели
+    list_display = ('id', 'first_name', 'last_name', 'email', 'phone_num', 'is_active' )
