026d0eab44ec26358b8caf7972e85a02d1a02001
DuckRainbow <sillestyna@gmail.com>
2025-01-20 15:56:03 +0500

    validator for supplier

--- retailing/validators.py
+++ retailing/validators.py
@@ -6,11 +6,13 @@ class SupplierValidator:
     Данный валидатор проверяет, чтобы при создании поставщика, его уровень был больше(ниже по иерархии),
     чем уровень его поставщика
     """
+
     def __init__(self, field1, field2):
         self.level = field1
         self.supplier = field2
 
     def __call__(self):
-        if self.level <= self.supplier.level:
-            raise ValidationError('Уровень должен быть больше уровня поставщика.')
+        if self.supplier:
+            if self.level <= self.supplier.level:
+                raise ValidationError('Уровень должен быть больше уровня поставщика.')
         return None
