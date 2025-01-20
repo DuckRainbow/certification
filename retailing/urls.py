5d67387df8d161f7b89db7dda0be8c79f713ab7a
DuckRainbow <sillestyna@gmail.com>
2025-01-20 14:17:25 +0500

    urls for retailing

--- retailing/urls.py
+++ retailing/urls.py
@@ -3,6 +3,21 @@ from rest_framework.routers import DefaultRouter
 
 from retailing.apps import RetailingConfig
 from retailing.views import (
+    SupplierListAPIView,
+    SupplierCreateAPIView,
+    SupplierRetrieveAPIView,
+    SupplierUpdateAPIView,
+    SupplierDestroyAPIView,
+    ContactListAPIView,
+    ContactCreateAPIView,
+    ContactRetrieveAPIView,
+    ContactUpdateAPIView,
+    ContactDestroyAPIView,
+    ProductListAPIView,
+    ProductCreateAPIView,
+    ProductRetrieveAPIView,
+    ProductUpdateAPIView,
+    ProductDestroyAPIView,
 )
 
 app_name = RetailingConfig.name
@@ -10,10 +25,19 @@ app_name = RetailingConfig.name
 router = DefaultRouter()
 
 urlpatterns = [
-                  path('', .as_view(), name='_list'),
-                  path('create/', .as_view(), name='_create'),
-                  path('my_/', .as_view(), name='_mylist'),
-                  path('<int:pk>/', .as_view(), name='_retrieve'),
-                  path('<int:pk>/update/', .as_view(), name='_update'),
-                  path('<int:pk>/delete/', .as_view(), name='_delete'),
+                  path('', SupplierListAPIView.as_view(), name='suppliers_list'),
+                  path('create/', SupplierCreateAPIView.as_view(), name='supplier_create'),
+                  path('<int:pk>/', SupplierRetrieveAPIView.as_view(), name='supplier_retrieve'),
+                  path('<int:pk>/update/', SupplierUpdateAPIView.as_view(), name='_update'),
+                  path('<int:pk>/delete/', SupplierDestroyAPIView.as_view(), name='_delete'),
+                  path('contacts/', ContactListAPIView.as_view(), name='contacts_list'),
+                  path('contacts/create/', ContactCreateAPIView.as_view(), name='contact_create'),
+                  path('contacts/<int:pk>/', ContactRetrieveAPIView.as_view(), name='contact_retrieve'),
+                  path('contacts/<int:pk>/update/', ContactUpdateAPIView.as_view(), name='contact_update'),
+                  path('contacts/<int:pk>/delete/', ContactDestroyAPIView.as_view(), name='contact_delete'),
+                  path('products/', ProductListAPIView.as_view(), name='products_list'),
+                  path('products/create/', ProductCreateAPIView.as_view(), name='product_create'),
+                  path('products/<int:pk>/', ProductRetrieveAPIView.as_view(), name='product_retrieve'),
+                  path('products/<int:pk>/update/', ProductUpdateAPIView.as_view(), name='product_update'),
+                  path('products/<int:pk>/delete/', ProductDestroyAPIView.as_view(), name='product_delete'),
               ] + router.urls
